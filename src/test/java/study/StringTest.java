569729227794fdfc2118dbb45a950a7b0253a264
Jaeyoung You <ruew12@gamil.com>
2021-06-08 00:36:28 +0900

    Test Learning for String Class Day 1 of Exercise Requirement No. 3 needs to be revised

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,13 +1,56 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class StringTest {
+
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void requirements1() {
+
+        List<Integer> actual = Arrays.stream("1,2".split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        Integer[] arr = actual.toArray(new Integer[actual.size()]);
+
+        assertThat(arr).contains(1, 2);
+
+        actual = Arrays.stream("1".split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        arr = actual.toArray(new Integer[actual.size()]);
+
+        assertThat(arr).containsExactly(1);
+    }
+
+    @Test
+    void requirements2() {
+
+        List<Integer> actual = Arrays.stream("(1,2)".substring(1,4).split(",")).map(Integer::parseInt).collect(Collectors.toList());
+        Integer[] arr = actual.toArray(new Integer[actual.size()]);
+
+        assertThat(arr).containsExactly(1, 2);
+    }
+
+    @Test
+    @DisplayName("index를 벗어났을 때 예외발생")
+    void requirements3() {
+
+        String str = "abc";
+
+        assertThatThrownBy(() -> {
+            str.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class);
     }
 }
